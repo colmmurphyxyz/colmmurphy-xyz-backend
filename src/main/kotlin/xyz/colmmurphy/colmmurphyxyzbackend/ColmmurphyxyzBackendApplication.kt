f10873fd51954f3fcb889d9a7e63b9001462e6de
Colm Murphy <colmmurphy016@gmail.com>
2025-03-30 14:13:10 +0100

    Initial commit

---
+++ src/main/kotlin/xyz/colmmurphy/colmmurphyxyzbackend/ColmmurphyxyzBackendApplication.kt
@@ -0,0 +1,11 @@
+package xyz.colmmurphy.colmmurphyxyzbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ColmmurphyxyzBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ColmmurphyxyzBackendApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/colmmurphy/colmmurphyxyzbackend/ColmmurphyxyzBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.colmmurphy.colmmurphyxyzbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ColmmurphyxyzBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
