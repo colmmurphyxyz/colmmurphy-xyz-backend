2181a32654c5bf00877eebc1625af7aee9d6e2d8
Colm Murphy <colm@colmmurphy.xyz>
2025-07-19 14:59:45 +0100

    nixify

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Developer Environment.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "spring boot + kotlin dev";
+          buildInputs = with pkgs; [
+            git
+            gradle
+            jdk17
+            kotlin
+          ];
+          shellHook = ''
+            echo "Activated"
+          '';
+        };
+      }
+    );
+}
+
