fa2bd6f83183bbd629adb4eea04acaba569d392f
Colm Murphy <colm@colmmurphy.xyz>
2025-07-29 19:27:08 +0100

    enable scheduling

--- src/main/kotlin/xyz/colmmurphy/colmmurphyxyzbackend/ColmmurphyxyzBackendApplication.kt
+++ src/main/kotlin/xyz/colmmurphy/colmmurphyxyzbackend/ColmmurphyxyzBackendApplication.kt
@@ -2,9 +2,15 @@ package xyz.colmmurphy.colmmurphyxyzbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 class ColmmurphyxyzBackendApplication
+
+@Configuration
+@EnableScheduling
+class SpringConfig
 
 fun main(args: Array<String>) {
     runApplication<ColmmurphyxyzBackendApplication>(*args)
